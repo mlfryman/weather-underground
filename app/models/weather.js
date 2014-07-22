3b3b7c6734e670a913c2140b9fccc30cc91020cc
Melanie Fryman <melanie@frymanet.com>
2014-07-22 13:50:18 -0500

    added average high and low temps over 10 days

--- app/models/weather.js
+++ app/models/weather.js
@@ -24,4 +24,36 @@ Weather.low = function(zip, cb){
   });
 };
 
+Weather.avgHigh = function(zip, cb){
+  var url = 'http://api.wunderground.com/api/f62d84d2f24e71aa/forecast10day/q/' + zip + '.json';
+  request(url, function(error, response, body){
+    body = JSON.parse(body);
+    var sum = 0;//set sum to 0 to add days (forecastday[]).
+    var days = body.forecast.simpleforecast.forecastday;
+
+      for(var i = 0; i < days.length; i++){//loop through days (forecastday[]).
+        sum += parseFloat(days[i].high.fahrenheit);
+      }
+
+    var avg = (sum / days.length); //calculate average 
+    cb(avg);//return average in callBack function
+  });
+};
+
+Weather.avgLow = function(zip, cb){
+  var url = 'http://api.wunderground.com/api/f62d84d2f24e71aa/forecast10day/q/' + zip + '.json';
+  request(url, function(error, response, body){
+    body = JSON.parse(body);
+    var sum = 0;
+    var days = body.forecast.simpleforecast.forecastday;//this is an alias that points to the forecast array
+
+    for(var i = 0; i < days.length; i++){
+      sum += parseInt(days[i].low.fahrenheit);
+    }
+
+    var avg = sum / days.length;
+    cb(avg);
+  });
+};
+
 module.exports = Weather;
--- test/unit/weather.js
+++ test/unit/weather.js
@@ -27,6 +27,54 @@ describe('Weather', function (){
     });
   });
 
+  describe('.avgHigh', function(){
+    it('should return the average high temp over 10 days', function(done){
+      Weather.avgHigh(37219, function(temp){
+       expect(temp).to.be.within(-50, 150);
+       console.log(temp);
+       done();
+      });
+    });
+  });
+
+  describe('.avgLow', function(){
+    it('should return the average low temp for today', function(done){
+      Weather.avgLow(37219, function(temp){
+       expect(temp).to.be.within(-50, 150);
+       done();
+      });
+    });
+  });
+
+  describe('.highs', function(){
+    it('should return an array of high temps over 10 days', function(done){
+      Weather.highs(37219, function(temp){
+       expect(temp).to.have.length(10);//array of high temps for 10 day forecast
+       console.log(temp);
+       done();
+      });
+    });
+  });
+
+  describe('.lows', function(){
+    it('should return an array of low temps over 10 days', function(done){
+      Weather.lows(37219, function(temp){
+       expect(temp).to.have.length(10);//array of low temps for 10 day forecast
+       console.log(temp);
+       done();
+      });
+    });
+  });
+
+  describe('.deltas', function(){
+    it('should return an array of the deltas of high & low temps over 10 days', function(done){
+      Weather.lows(37219, function(temp){
+       expect(temp).to.have.length(10);//array of low temps for 10 day forecast
+       console.log(temp);
+       done();
+      });
+    });
+  });
 
 });
 
